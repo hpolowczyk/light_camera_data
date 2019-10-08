2e97108fc9eacf689bc166dea54e689a4e11d729
hpolowczyk <50588161+hpolowczyk@users.noreply.github.com>
2019-10-07 20:22:16 -0400

    cleaned up css

--- flask_website/static/js/chord_app.js
+++ flask_website/static/js/chord_app.js
@@ -3,27 +3,29 @@ const data = '/json';
 
 // Using d3, fetch the JSON data
 d3.json(data).then((data) => {
-    var movieData = [];
     //  console.log(data) 
     var button = d3.select("#filter-btn")
 
-    button.on("click",() => {
+    button.on("click", () => {
         var inputStart = d3.select("#start");
         var startValue = inputStart.property("value")
-        var startValue = startValue-1;
+        var startValue = startValue - 1;
         var inputEnd = d3.select("#end");
         var endValue = inputEnd.property("value");
         console.log(startValue)
-        buildCSV(startValue,endValue)
-        }
+        buildCSV(startValue, endValue, data)
+    }
     )
+    buildCSV(0, 25, data);
+});
 
-    function buildCSV(start,end) {
-    data.slice(start,end).forEach((movie) => {
+function buildCSV(start, end, data) {
+    var movieData = [];
+    data.slice(start, end).forEach((movie) => {
         var actors = movie.Actors;
-       // var genres = movie.Genre;
+        // var genres = movie.Genre;
         actors.forEach((actor) => {
-            var movieList = [movie.Title,movie.Studio];
+            var movieList = [movie.Title, movie.Studio];
             movieList.push(actor);
             movieData.push(movieList);
         })
@@ -31,8 +33,9 @@ d3.json(data).then((data) => {
     //console.log(movieData);
     // From JSON
     var json = {
-        'header' : [ 'Movies', 'Studio', 'Actor' ],
-        'data'   : movieData };
+        'header': ['Movies', 'Studio', 'Actor'],
+        'data': movieData
+    };
     var csv = new dex.csv(json);
 
 
@@ -40,14 +43,7 @@ d3.json(data).then((data) => {
     var network = dex.charts.d3plus.RingNetwork({
         'parent': '#D3Plus_RingNetwork',
         'type': 'rings',
-        'edges': {'arrows': true},
+        'edges': { 'arrows': true },
         'csv': csv
     }).render()
 }
-
-    
-});
-
-
-
-
