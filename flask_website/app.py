8811d4720ba36506b339cb19c0b9ecd1b0e20358
Manoel Britto <manoelbritto@gmail.com>
2019-10-08 20:44:50 -0400

    including map

--- flask_website/app.py
+++ flask_website/app.py
@@ -1,19 +1,22 @@
-from flask import Flask, render_template,jsonify
+from flask import Flask, render_template, jsonify
 import pymongo
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
 # setup mongo connection
-mongo = PyMongo(app, uri="mongodb+srv://generaluser:generaluser123@project2-ha8my.mongodb.net/movie_db?retryWrites=true&w=majority")
+mongo = PyMongo(
+    app, uri="mongodb+srv://generaluser:generaluser123@project2-ha8my.mongodb.net/movie_db?retryWrites=true&w=majority")
+
 
 @app.route('/json')
 def jsonified():
     docs = []
     for doc in mongo.db.movie_detail.find():
-        doc.pop('_id') 
+        doc.pop('_id')
         docs.append(doc)
     return jsonify(docs)
+
 
 @app.route('/movie_ring')
 def movie_ring():
@@ -23,6 +26,7 @@ def movie_ring():
     # render an index.html template and pass it the data you retrieved from the database
     return render_template("movie_ring.html", inventory=inventory)
 
+
 @app.route('/')
 def index():
     # write a statement that finds all the items in the db and sets it to a variable
@@ -30,6 +34,7 @@ def index():
 
     # render an index.html template and pass it the data you retrieved from the database
     return render_template("index.html", inventory=inventory)
+
 
 @app.route('/bubble')
 def bubble():
@@ -48,6 +53,7 @@ def word_cloud():
     # render an word_cloud.html template and pass it the data you retrieved from the database
     return render_template("word_cloud.html", inventory=inventory)
 
+
 @app.route('/sankey')
 def sankey():
     # write a statement that finds all the items in the db and sets it to a variable
@@ -56,6 +62,7 @@ def sankey():
     # render an index.html template and pass it the data you retrieved from the database
     return render_template("sankey.html", inventory=inventory)
 
+
 @app.route('/treemap')
 def treemap():
     # write a statement that finds all the items in the db and sets it to a variable
@@ -63,6 +70,35 @@ def treemap():
 
     # render an treemap.html template and pass it the data you retrieved from the database
     return render_template("treemap.html", inventory=inventory)
+
+
+@app.route("/map")
+def init():
+    return render_template("map.html")
+
+
+@app.route("/filterLessRange_IG_Rank/<value>")
+def filterRankRange(value):
+    # return items from a different collection (international_gross_det) base on the rank
+    value = int(value)
+    docs = []
+    # select data less or equal the rank selected
+    for doc in mongo.db.international_gross_det.find({'rank': {'$lte': value}}):
+        doc.pop('_id')
+        docs.append(doc)
+    return jsonify(docs)
+
+
+@app.route("/filterLessEq_IG_Rank/<value>")
+def filterRank(value):
+    # return items from a different collection (international_gross_det) base on the rank
+    value = int(value)
+    docs = []
+    # select data equal the rank selected
+    for doc in mongo.db.international_gross_det.find({'rank': value}):
+        doc.pop('_id')
+        docs.append(doc)
+    return jsonify(docs)
 
 
 if __name__ == "__main__":
