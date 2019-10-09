126fa403def27203de5f37b30a77bec8e9ae13bd
hpolowczyk <50588161+hpolowczyk@users.noreply.github.com>
2019-10-09 19:01:08 -0400

    cleaned up folders

--- FlaskPage/generalFunctions.py
+++
@@ -1,6 +0,0 @@
-# Dependencies
-from splinter import Browser
-from bs4 import BeautifulSoup as bs
-import time
-import pandas as pd
-import pymongo
--- FlaskPage/static/js/my_key.js
+++
@@ -1,2 +0,0 @@
-const API_KEY =
-  "pk.eyJ1IjoibWFub2VsYnJpdHRvIiwiYSI6ImNrMHRtcWptNDBmNXIzaHBvbHppNzFjcTYifQ.lkiyJlLT61s8B8_0MBsiOg";
